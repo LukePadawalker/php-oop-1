70942c6a865f9b245ec6fdcacaaa9886d5c6e871
LukePadawalker <padurariu.luca@gmail.com>
2024-02-23 18:29:10 +0200

    exercise done

--- index.php
+++ index.php
@@ -5,15 +5,35 @@ class Movie
     public $name;
     public $genre;
 
+    public $releaseYear;
+
+
+    public function __construct($name, $genre, $releaseYear)
+    {
+        $this->name = $name;
+        $this->genre = $genre;
+        $this->releaseYear = $releaseYear;
+    }
+
+
+    public function returnName()
+    {
+        return $this->name;
+    }
+    public function returnGenre()
+    {
+        return $this->genre;
+    }
+    public function returnReleaseYear()
+    {
+        return $this->releaseYear;
+    }
 }
 
-
-
+$movie1 = new Movie("Inception", "Sci-Fi", 2010);
+$movie2 = new Movie("Oppenhenimer", "Historical", 2023);
 
 ?>
-
-
-
 
 
 <!DOCTYPE html>
@@ -27,7 +47,14 @@ class Movie
 </head>
 
 <body>
-    <div></div>
+    <div>
+        <p>
+            <?php echo $movie1->returnName() . ' ' . $movie1->returnGenre() . ' ' . $movie1->returnReleaseYear() ?>
+        </p>
+        <p>
+            <?php echo $movie2->returnName() . ' ' . $movie2->returnGenre() . ' ' . $movie2->returnReleaseYear() ?>
+        </p>
+    </div>
 
 </body>
 
